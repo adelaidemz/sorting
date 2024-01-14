968a5f39494d5fc5b823053e3bb32700688abba2
Adelaide Zhang <adelaidemzhang@gmail.com>
2024-01-14 17:44:22 -0500

    set full width

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,7 @@ function App() {
     const [statuses, setStatus] = useState<STATUS[]>(Array(algorithms.length).fill("default"));
     
     return (
-        <VStack h="100%" alignItems="center" justifyContent={"center"} spacing={4}>
+        <VStack h="100%" w="100%" alignItems="center" justifyContent={"center"} spacing={4}>
             <p>This is a quick visualization of a few iterative sorting methods.</p>
             <p>Note: each step takes a fixed amount of time for visibility, so the algorithms' actual runtimes are not accounted for. </p>
                 {/* <button onClick={() => console.log(statuses)}>Print Statuses</button> */}
